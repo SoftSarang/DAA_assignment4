6dbd2910a4c128adef1321ddb04d246b33a16c09
KapDan04 <dkaparov0@gmail.com>
2025-10-30 16:29:15 +0500

    ts: implement Kahn's topological sort algorithm

---
+++ src/main/java/com/aitu/ts/TopologicalSortResult.java
@@ -0,0 +1,23 @@
+package com.aitu.ts;
+
+import java.util.List;
+
+
+public class TopologicalSortResult {
+    private final List<Integer> order;
+    private final boolean isDAG;
+
+    public TopologicalSortResult(List<Integer> order, boolean isDAG) {
+        this.order = order;
+        this.isDAG = isDAG;
+    }
+
+    public List<Integer> getOrder() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return isDAG;
+    }
+}
+
